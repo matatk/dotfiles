0ab843950830b5aa7d54622bb4797b23c58851de
Matthew Tylee Atkinson <matatk@gmail.com>
2014-12-09 16:26:29 +0000

    Add homebrew sbin to path

--- shell/environment.sh
+++ shell/environment.sh
@@ -1,5 +1,5 @@
 PREFIX=`brew --prefix`
-export PATH=$HOME/bin:$PREFIX/bin:$PATH
+export PATH=$HOME/bin:$PREFIX/bin:$PREFIX/sbin:$PATH
 export EDITOR=`which vim`  # used by git commit et al
 export XML_CATALOG_FILES="$PREFIX/etc/xml/catalog"  # for DocBook
 source $PREFIX/opt/autoenv/activate.sh
