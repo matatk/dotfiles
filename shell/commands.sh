020e5a644a69eccd17a008af820bbb3a224e7fa1
Matthew Tylee Atkinson <matatk@gmail.com>
2018-08-06 14:24:37 +0100

    Un-clobber the "nl" command
    Learned a new UNIX command! Thanks http://vim.wikia.com/wiki/Insert_line_numbers :-)

--- shell/commands.sh
+++ shell/commands.sh
@@ -52,7 +52,7 @@ alias brewall='brewup && echo && brewdo'
 
 # NPM package listing stuff
 # https://gist.github.com/yyx990803/6045243
-alias nl="npm list --depth=0 2>/dev/null"
+alias npml="npm list --depth=0 2>/dev/null"
 
 # Add a file to an existing MacVim tab
 alias mvt='mvim --remote-tab'
