f68f516bf3e8172f52d651db9b5457837630342b
Matthew Tylee Atkinson <matatk@gmail.com>
2019-07-24 19:54:27 +0100

    stylelint; emsdk; ignores
    * Use stylelint with syntastic.
    * Emscripten SDK environment variables.
    * Ignore package-lock.json in ~/bin

--- shell/environment.sh
+++ shell/environment.sh
@@ -19,4 +19,8 @@ export LDFLAGS="-L/usr/local/opt/ruby/lib"
 export CPPFLAGS="-I/usr/local/opt/ruby/include"
 export PKG_CONFIG_PATH="/usr/local/opt/ruby/lib/pkgconfig"
 
+if [ -r ~/projects/emsdk/emsdk_env.sh ]; then
+	source ~/projects/emsdk/emsdk_env.sh > /dev/null
+fi
+
 export PATH="$HOME/.cabal/bin:$PATH"
