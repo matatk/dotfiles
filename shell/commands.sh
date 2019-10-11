b7fd76ac031b065b9c04688d8d9611923f7bfa0b
Matthew Tylee Atkinson <matatk@gmail.com>
2019-10-11 16:48:33 +0100

    Update for Sigil 1.21

--- shell/commands.sh
+++ shell/commands.sh
@@ -56,7 +56,7 @@ alias slt='stylelint --config-basedir /usr/local/lib/node_modules/stylelint/'
 # Zandronum
 alias zandronum='/Applications/Zandronum.app/Contents/MacOS/zandronum'
 alias zd1='zandronum -iwad doom'
-alias zd1s='zandronum -iwad doom -file sigil'
+alias zd1s='zandronum -iwad doom -file sigil_v1_21'
 alias zsd1scoopuv='zandronum -private -host 2 -skill 4 -iwad doom -file sigil'
 
 alias renpm='rm -rf package-lock.json node_modules && npm install'
