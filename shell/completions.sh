7697863ac11e992356428f66d52b44fe048455c5
Matthew Tylee Atkinson <matatk@gmail.com>
2021-01-08 14:55:51 +0000

    Note error with bash

--- shell/completions.sh
+++ shell/completions.sh
@@ -1,4 +1,4 @@
-command -v npm > /dev/null && source <(npm completion)
+command -v npm > /dev/null && source <(npm completion)  # FIXME: bash error
 
 # adapted from that which was added automatically by the travis gem
 [ -f ~/.travis/travis.sh ] && source ~/.travis/travis.sh
