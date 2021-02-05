62dfe337f3fffff4743c2314f60ab3b0707cf01f
Matthew Tylee Atkinson <matatk@gmail.com>
2021-02-05 13:50:19 +0000

    Drop Travis; Remove note about Bash error
    Still not sure if npm completion is working as intended in bash though.

--- shell/completions.sh
+++ shell/completions.sh
@@ -1,4 +1,1 @@
-command -v npm > /dev/null && source <(npm completion)  # FIXME: bash error
-
-# adapted from that which was added automatically by the travis gem
-[ -f ~/.travis/travis.sh ] && source ~/.travis/travis.sh
+command -v npm > /dev/null && source <(npm completion)
