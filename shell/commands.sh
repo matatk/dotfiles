f14d065faad56d9422c301d2b3a17d914a7c1133
Matthew Tylee Atkinson <matatk@gmail.com>
2022-07-26 20:27:10 +0100

    Remove the tidysnippet command

--- shell/commands.sh
+++ shell/commands.sh
@@ -152,28 +152,6 @@ function _mdpretty() {
 	fi
 }
 
-# Tidy a snippet of HTML that was placed on the clipboard
-function tidysnippet() {
-	CORE_TIDY='pbpaste | tidy --indent auto --indent-spaces 4 --wrap 0 --show-body-only yes'
-	echo 'Tidying:'
-	pbpaste | cat
-	echo
-	echo
-	eval "$CORE_TIDY"
-	RETVAL=$?
-	echo
-	if [ ! $RETVAL -eq 2 ]; then
-		eval "$CORE_TIDY" 2>/dev/null | pbcopy
-		echo 'The above result of tidy is now on the clipboard.'
-		if [ $RETVAL -eq 1 ]; then
-			echo
-			echo 'Note: there were warnings from tidy (but no errors).'
-		fi
-	else
-		echo 'There was an error calling tidy; clipboard unchanged.'
-	fi
-}
-
 function pretty_tsv {
     column -t -s $'\t'
 }
