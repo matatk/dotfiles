e01b9e78d32cbb50288bcd4b4e38d6d08887e1e7
Matthew Tylee Atkinson <matatk@gmail.com>
2023-02-11 16:31:24 +0000

    colours-restructure-plugins

--- nvim/.config/nvim/lua/plugins/colours.lua
+++ nvim/.config/nvim/lua/plugins/colours.lua
@@ -1,9 +1,7 @@
 return {
 	--
-	-- Colours
-	--
-
 	-- Not TreeSitter-supporting
+	--
 
 	-- Molokai
 	{
@@ -14,9 +12,16 @@ return {
 		end
 	},
 
+	--
 	-- TreeSitter-supporting
+	--
+
 	-- https://github.com/rockerBOO/awesome-neovim#colorscheme
 	-- https://www.reddit.com/r/neovim/comments/srifm4/the_best_neovim_color_scheme_with_tree_sitter_and/
+
+	{ 'morhetz/gruvbox' },
+
+	{ 'dracula/vim' },
 
 	{
 		'folke/tokyonight.nvim',
@@ -47,7 +52,6 @@ return {
 		end
 	},
 
-
 	-- https://github.com/material-theme/vsc-material-theme#official-portings
 	{
 		'kaicataldo/material.vim',
@@ -70,53 +74,4 @@ return {
 			--vim.g.material_style = 'oceanic'
 		end
 	},
-
-	--
-	-- Classic bangin' Vim plugins
-	--
-
-	'tpope/vim-surround',
-	'mbbill/undotree',
-
-	--
-	-- NeoVim plugins
-	--
-
-	-- https://github.com/nvim-treesitter/nvim-treesitter/wiki/Installation#packernvim
-	{ 'nvim-treesitter/nvim-treesitter', build = ":TSUpdate" },
-	{ 'nvim-treesitter/playground' }, -- TODO: depends on treesitter; express that here?
-
-	-- Telescope
-	{
-		'nvim-telescope/telescope.nvim',
-		branch = '0.1.x',
-		dependencies = {
-			'nvim-lua/plenary.nvim', -- TODO: tag = '0.1.2' ?
-			{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
-		}
-	},
-
-	-- LSP Zero
-	{
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v1.x',
-		dependencies = {
-			-- LSP Support
-			{ 'neovim/nvim-lspconfig' }, -- Required
-			{ 'williamboman/mason.nvim' }, -- Optional
-			{ 'williamboman/mason-lspconfig.nvim' }, -- Optional
-
-			-- Autocompletion
-			{ 'hrsh7th/nvim-cmp' }, -- Required
-			{ 'hrsh7th/cmp-nvim-lsp' }, -- Required
-			{ 'hrsh7th/cmp-buffer' }, -- Optional
-			{ 'hrsh7th/cmp-path' }, -- Optional
-			{ 'saadparwaiz1/cmp_luasnip' }, -- Optional
-			{ 'hrsh7th/cmp-nvim-lua' }, -- Optional
-
-			-- Snippets
-			{ 'L3MON4D3/LuaSnip' }, -- Required
-			{ 'rafamadriz/friendly-snippets' }, -- Optional
-		}
-	}
 }
---
+++ nvim/.config/nvim/lua/plugins/others.lua
@@ -0,0 +1,50 @@
+return {
+	--
+	-- Classic bangin' Vim plugins
+	--
+
+	'tpope/vim-surround',
+	'mbbill/undotree',
+
+	--
+	-- NeoVim plugins
+	--
+
+	-- https://github.com/nvim-treesitter/nvim-treesitter/wiki/Installation#packernvim
+	{ 'nvim-treesitter/nvim-treesitter', build = ":TSUpdate" },
+	{ 'nvim-treesitter/playground' }, -- TODO: depends on treesitter; express that here?
+
+	-- Telescope
+	{
+		'nvim-telescope/telescope.nvim',
+		branch = '0.1.x',
+		dependencies = {
+			'nvim-lua/plenary.nvim', -- TODO: tag = '0.1.2' ?
+			{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+		}
+	},
+
+	-- LSP Zero
+	{
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v1.x',
+		dependencies = {
+			-- LSP Support
+			{ 'neovim/nvim-lspconfig' }, -- Required
+			{ 'williamboman/mason.nvim' }, -- Optional
+			{ 'williamboman/mason-lspconfig.nvim' }, -- Optional
+
+			-- Autocompletion
+			{ 'hrsh7th/nvim-cmp' }, -- Required
+			{ 'hrsh7th/cmp-nvim-lsp' }, -- Required
+			{ 'hrsh7th/cmp-buffer' }, -- Optional
+			{ 'hrsh7th/cmp-path' }, -- Optional
+			{ 'saadparwaiz1/cmp_luasnip' }, -- Optional
+			{ 'hrsh7th/cmp-nvim-lua' }, -- Optional
+
+			-- Snippets
+			{ 'L3MON4D3/LuaSnip' }, -- Required
+			{ 'rafamadriz/friendly-snippets' }, -- Optional
+		}
+	}
+}
