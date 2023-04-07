16710dbaa74827f637c9db9d49b983a423346f88
Matthew Tylee Atkinson <matatk@gmail.com>
2023-04-07 11:36:09 +0100

    very cool telescope helpers

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -11,14 +11,17 @@ return {
 
 			-- https://github.com/nvim-telescope/telescope.nvim#usage
 			local builtin = require('telescope.builtin')
+			vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+			vim.keymap.set('n', '<leader>fc', builtin.colorscheme, {})
 			vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 			vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-			vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 			vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-
 			vim.keymap.set('n', '<leader>fk', builtin.keymaps, {})
+			vim.keymap.set('n', '<leader>fr', builtin.lsp_references, {})
+			vim.keymap.set('n', '<leader>fs', builtin.lsp_document_symbols, {})
+			vim.keymap.set('n', '<leader>fS', builtin.lsp_dynamic_workspace_symbols, {})
+
 			vim.keymap.set('n', '<leader>fB', builtin.builtin, {})
-			vim.keymap.set('n', '<leader>fc', builtin.colorscheme, {})
 		end
 	}
 }
