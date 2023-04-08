e9b0a1401bf2def8397159a5ebac40e60ef45a85
Matthew Tylee Atkinson <matatk@gmail.com>
2023-04-08 18:44:34 +0100

    use devicons for telescope, too

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -4,7 +4,8 @@ return {
 		branch = '0.1.x',
 		dependencies = {
 			'nvim-lua/plenary.nvim', -- TODO: tag = '0.1.2' ?
-			{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+			{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+			"nvim-tree/nvim-web-devicons",
 		},
 		config = function()
 			require('telescope').load_extension('fzf')
