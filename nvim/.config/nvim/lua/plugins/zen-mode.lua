a8bd791c58043332f0b6b7455a7a1ac77a70cea1
Matthew Tylee Atkinson <matatk@gmail.com>
2023-07-10 21:22:04 +0100

    [vim] Zen mode size shortcuts

--- nvim/.config/nvim/lua/plugins/zen-mode.lua
+++ nvim/.config/nvim/lua/plugins/zen-mode.lua
@@ -2,14 +2,18 @@
 return {
 	"folke/zen-mode.nvim",
 	config = function()
-		require("zen-mode").setup {
-			window = {
-				width = 80
+		local core = function(columns)
+			require("zen-mode").toggle {
+				window = {
+					width = columns
+				}
 			}
-		}
+		end
 
 		-- Added by me
 		-- TODO: make silent etc. and DRY that
-		vim.keymap.set('n', '<Leader>z', vim.cmd.ZenMode)
+		vim.keymap.set('n', '<Leader>z8', function() core(80) end)
+		vim.keymap.set('n', '<Leader>z9', function() core(90) end)
+		vim.keymap.set('n', '<Leader>z1', function() core(100) end)
 	end
 }
