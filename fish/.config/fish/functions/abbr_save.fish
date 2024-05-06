58b48327887275695d4a8c84a1b5302a83b507b7
Matthew Tylee Atkinson <matatk@gmail.com>
2024-05-06 17:55:39 +0100

    [fish] Plugins; Aliases
    * Remove z plugin
    * Add aliases for cargo stuff; including offline variants
    * Add an alias to reset the terminal first before attempting build and
      install

--- fish/.config/fish/functions/abbr_save.fish
+++ fish/.config/fish/functions/abbr_save.fish
@@ -1,8 +1,8 @@
 # Based on https://www.reddit.com/r/fishshell/comments/k2zx7m/comment/gdytxlr/
 function abbr_save --description 'Save the definition of all abbreviations'
     set -l abbr_init $HOME/.config/fish/functions/abbr_init.fish
-    echo "function abbr_init --description 'Initializes abbreviations'" > "$abbr_init"
-    abbr | sort >> "$abbr_init"
-    echo end >> "$abbr_init"
+    echo "function abbr_init --description 'Initializes abbreviations'" >"$abbr_init"
+    abbr | sort >>"$abbr_init"
+    echo end >>"$abbr_init"
     fish_indent -w "$abbr_init"
 end
--- fish/.config/fish/functions/universal_setup.fish
+++ fish/.config/fish/functions/universal_setup.fish
@@ -1,20 +1,21 @@
 function universal_setup --description 'Set things up after a reinstall'
-	fish_vi_key_bindings
+    fish_vi_key_bindings
 
-	set -Ux EDITOR hx
-	set -Ux BAT_THEME Dracula
+    set -Ux EDITOR hx
+    set -Ux BAT_THEME Dracula
+    set -Ux DOOMWADPATH /Users/matatk/WADs
 
-	if command --query /opt/homebrew/bin/brew
-		set -f prefix /opt/homebrew
-		fish_add_path $prefix/bin
-	else if command --query /usr/local/bin/brew
-		set -f prefix /usr/local
-	end
-	fish_add_path $prefix/sbin
-	fish_add_path $prefix/opt/node@20/bin
+    if command --query /opt/homebrew/bin/brew
+        set -f prefix /opt/homebrew
+        fish_add_path $prefix/bin
+    else if command --query /usr/local/bin/brew
+        set -f prefix /usr/local
+    end
+    fish_add_path $prefix/sbin
+    fish_add_path $prefix/opt/node@20/bin
 
-	fish_add_path ~/.cargo/bin
-	fish_add_path ~/bin
+    fish_add_path ~/.cargo/bin
+    fish_add_path ~/bin
 
-	echo -e "If running this after updating any paths, clear out the existing paths first:\nset -e fish_user_paths"
+    echo -e "If running this after updating any paths, clear out the existing paths first:\nset -e fish_user_paths"
 end
